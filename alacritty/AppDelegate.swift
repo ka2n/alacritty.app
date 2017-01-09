5fac989756a44ce8fe9b12ad05afd5d1f898a100
Katsuma Ito <katsumai@gmail.com>
2017-01-09 20:42:16 +0900

    Initial Commit

---
+++ alacritty/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  alacritty
+//
+//  Created by ka2n on 2017/01/09.
+//  Copyright © 2017 Katsuma Ito. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
